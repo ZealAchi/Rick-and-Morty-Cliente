7be08b8ad3207ed19d93832778479e61a4b864e0
luispadre <luis.padre@protonmail.com>
2019-07-11 14:21:36 -0500

    Cambios

---
+++ client/precache-manifest.d0028ab173ba3bac707ab36db5bdc3da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2332c8a7b47eafb7eec765c596e199e5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a996d680c32cf9afeb17",
+    "url": "/static/css/2.156e8e33.chunk.css"
+  },
+  {
+    "revision": "86d563b5ee3827c83d5e",
+    "url": "/static/css/main.498d002a.chunk.css"
+  },
+  {
+    "revision": "a996d680c32cf9afeb17",
+    "url": "/static/js/2.b20d0d3c.chunk.js"
+  },
+  {
+    "revision": "86d563b5ee3827c83d5e",
+    "url": "/static/js/main.f401465b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
